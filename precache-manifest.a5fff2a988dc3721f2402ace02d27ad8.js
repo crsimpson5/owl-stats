8907bfb7d2f3cbe3b91d7b734008ac182eb488d3
Curtis Simpson <crsimpson5@gmail.com>
2019-05-28 14:44:12 -0500

    Updates

---
+++ precache-manifest.a5fff2a988dc3721f2402ace02d27ad8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3c871a396d563a4800871fe2f2b8a87",
+    "url": "/owl-stats/index.html"
+  },
+  {
+    "revision": "64af9f6258012e3e3475",
+    "url": "/owl-stats/static/css/main.abbe4f72.chunk.css"
+  },
+  {
+    "revision": "9cdc56978471ca09bde6",
+    "url": "/owl-stats/static/js/2.75e63c91.chunk.js"
+  },
+  {
+    "revision": "64af9f6258012e3e3475",
+    "url": "/owl-stats/static/js/main.e68a345e.chunk.js"
+  },
+  {
+    "revision": "57f2ea25b09cc25cee66",
+    "url": "/owl-stats/static/js/runtime~main.0caba6ec.js"
+  }
+]);
